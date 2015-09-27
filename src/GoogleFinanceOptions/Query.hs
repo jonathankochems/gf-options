cf38fb65a8438461bd57edc49378fa82da5d277d
Jonathan Kochems <jonathan.kochems@gmail.com>
2015-09-27 19:41:26 +0100

    a bit of cleanup (#3)

--- src/GoogleFinanceOptions/Query.hs
+++ src/GoogleFinanceOptions/Query.hs
@@ -22,7 +22,12 @@ module GoogleFinanceOptions.Query(
   -- * Inspecting RawOptionQueryResult
   expiryDateForQuery, availableExpirations, puts, calls, underlying, queryUnderlyingSpotprice, 
   -- * Low-level functions
-  rawRequest, parseRawOptionQueryResult, gfinanceUrl ) where
+  rawRequest, parseRawOptionQueryResult, gfinanceUrl, 
+  -- $ Helper functions
+  lPutFromJSONPut, lCallFromJSONCall, lFromJSString, lFromJSOBJECT, 
+  lFromJSDate, lFromMaybeJSDouble, lFromMaybeJSInt, lFromJSDouble, 
+  lFromJSInt, lFromJSRational, lFromJSSTRING, lDateFromString
+  ) where
 
 import Control.Applicative ((<$>))
 import Control.Lens ((.~))
@@ -32,13 +37,12 @@ import qualified Network.Browser as Browser
 import Data.Time.Calendar
 
 import Text.JSON(resultToEither)
-import Text.JSON.Types (JSValue(..), JSObject(..), get_field, fromJSString)
+import Text.JSON.Types (JSString(..), JSValue(..), JSObject(..), get_field, fromJSString)
 import Text.JSON.Parsec(runParser, spaces, many, string, digit, choice, try, CharParser(..))
 import Data.Maybe(fromMaybe, isNothing, fromJust)
 
 import Text.JSON.Permissive(decodePermissive)
 import GoogleFinanceOptions.Types (Call, Put, uninitialisedCall, uninitialisedPut, Contract(..), Result(..), fromResult)
---import GoogleFinanceOptions.Types (Contract) 
 import qualified GoogleFinanceOptions.Types as Types
 
 
@@ -91,31 +95,31 @@ parseRawOptionQueryResult doc = either (\e -> Error $ "parsing of raw option cha
 
 -- | Returns the expiry date that was used in the Google Finance Option Chain query.
 expiryDateForQuery :: RawOptionQueryResult -> Day                                
-expiryDateForQuery raw_oc = fromJSDate exp_obj
+expiryDateForQuery raw_oc = lFromJSDate exp_obj
     where Just (JSObject exp_obj) = get_field jsonobject "expiry"
           jsonobject = unwrapRawOptionQueryResult raw_oc 
 
 -- | Returns the available expiry dates that can be further queried for.
 availableExpirations :: RawOptionQueryResult -> [Day]
-availableExpirations raw_oc = map (fromJSDate . fromJSOBJECT) expirations_list
+availableExpirations raw_oc = map (lFromJSDate . lFromJSOBJECT) expirations_list
     where Just (JSArray expirations_list) = get_field jsonobject "expirations"
           jsonobject = unwrapRawOptionQueryResult raw_oc 
 
 -- | Returns all put options that expire on the queried date
 puts :: RawOptionQueryResult -> [Put]
-puts raw_oc = map (putFromJSONPut (queryUnderlyingSpotprice raw_oc) . fromJSOBJECT) put_list
+puts raw_oc = map (lPutFromJSONPut (queryUnderlyingSpotprice raw_oc) . lFromJSOBJECT) put_list
     where Just (JSArray put_list) = get_field jsonobject "puts"
           jsonobject = unwrapRawOptionQueryResult raw_oc 
 
 -- | Returns all call options that expire on the queried date
 calls :: RawOptionQueryResult -> [Call]
-calls raw_oc = map (callFromJSONCall (queryUnderlyingSpotprice raw_oc) . fromJSOBJECT) call_list
+calls raw_oc = map (lCallFromJSONCall (queryUnderlyingSpotprice raw_oc) . lFromJSOBJECT) call_list
     where Just (JSArray call_list) = get_field jsonobject "calls"
           jsonobject = unwrapRawOptionQueryResult raw_oc 
 
 -- | Returns the id of that underlying 
 underlying :: RawOptionQueryResult -> String
-underlying raw_oc = fromMyString "underlying_id" $ get_field jsonobject "underlying_id"
+underlying raw_oc = lFromJSString "underlying_id" $ get_field jsonobject "underlying_id"
      where jsonobject = unwrapRawOptionQueryResult raw_oc 
 
 -- | Returns the spotprice of the underlying 
@@ -126,89 +130,93 @@ queryUnderlyingSpotprice raw_oc = fromRational p  ::Double
 
 
 {--------------------------------------------------------------------
-  Helper functions
+  $Helper functions
 --------------------------------------------------------------------}
--- Converts a JSON object representing a put to an object of type Put
-putFromJSONPut spot jsonput = 
-    contractId          .~ fromMyString "cid"    (get_field jsonput "cid") $
-    name                .~ fromMyString "name"   (get_field jsonput "name") $
-    symbol              .~ fromMyString "s"      (get_field jsonput "s") $
-    exchange            .~ fromMyString "e"      (get_field jsonput "e") $
-    price               .~ fromMyDouble'         (get_field jsonput "p") $
-    change              .~ fromMyDouble'         (get_field jsonput "c" ) $
-    bid                 .~ fromMyDouble'         (get_field jsonput "b") $
-    ask                 .~ fromMyDouble'         (get_field jsonput "a") $
-    openinterest        .~ fromMyInt "oi"        (get_field jsonput "oi") $
-    volume              .~ fromMyInt'            (get_field jsonput "vol") $
-    strike              .~ fromMyDouble "strike" (get_field jsonput "strike") $
-    expiry              .~ dateFromString        (fromMyString "expiry" $ get_field jsonput "expiry") $
+-- | Converts a JSON object representing a put to an object of type Put
+lPutFromJSONPut :: Double -> JSObject JSValue -> Put
+lPutFromJSONPut spot jsonput = 
+    contractId          .~ lFromJSString "cid"    (get_field jsonput "cid") $
+    name                .~ lFromJSString "name"   (get_field jsonput "name") $
+    symbol              .~ lFromJSString "s"      (get_field jsonput "s") $
+    exchange            .~ lFromJSString "e"      (get_field jsonput "e") $
+    price               .~ lFromJSDouble          (get_field jsonput "p") $
+    change              .~ lFromJSDouble          (get_field jsonput "c" ) $
+    bid                 .~ lFromJSDouble          (get_field jsonput "b") $
+    ask                 .~ lFromJSDouble          (get_field jsonput "a") $
+    openinterest        .~ lFromMaybeJSInt "oi"        (get_field jsonput "oi") $
+    volume              .~ lFromJSInt            (get_field jsonput "vol") $
+    strike              .~ lFromMaybeJSDouble "strike" (get_field jsonput "strike") $
+    expiry              .~ lDateFromString        (lFromJSString "expiry" $ get_field jsonput "expiry") $
     underlyingSpotprice .~ spot $
     uninitialisedPut
 
--- Converts a JSON object representing a call to an object of type Call
-callFromJSONCall spot jsonput = 
-    contractId          .~ fromMyString "cid"    (get_field jsonput "cid") $
-    name                .~ fromMyString "name"   (get_field jsonput "name") $
-    symbol              .~ fromMyString "s"      (get_field jsonput "s") $
-    exchange            .~ fromMyString "e"      (get_field jsonput "e") $
-    price               .~ fromMyDouble'         (get_field jsonput "p") $
-    change              .~ fromMyDouble'         (get_field jsonput "c" ) $
-    bid                 .~ fromMyDouble'         (get_field jsonput "b") $
-    ask                 .~ fromMyDouble'         (get_field jsonput "a") $
-    openinterest        .~ fromMyInt "oi"        (get_field jsonput "oi") $
-    volume              .~ fromMyInt'            (get_field jsonput "vol") $
-    strike              .~ fromMyDouble "strike" (get_field jsonput "strike") $
-    expiry              .~ dateFromString        (fromMyString "expiry" $ get_field jsonput "expiry") $
+-- | Converts a JSON object representing a call to an object of type Call
+lCallFromJSONCall :: Double -> JSObject JSValue -> Call
+lCallFromJSONCall spot jsonput = 
+    contractId          .~ lFromJSString "cid"    (get_field jsonput "cid") $
+    name                .~ lFromJSString "name"   (get_field jsonput "name") $
+    symbol              .~ lFromJSString "s"      (get_field jsonput "s") $
+    exchange            .~ lFromJSString "e"      (get_field jsonput "e") $
+    price               .~ lFromJSDouble          (get_field jsonput "p") $
+    change              .~ lFromJSDouble          (get_field jsonput "c" ) $
+    bid                 .~ lFromJSDouble          (get_field jsonput "b") $
+    ask                 .~ lFromJSDouble          (get_field jsonput "a") $
+    openinterest        .~ lFromMaybeJSInt "oi"        (get_field jsonput "oi") $
+    volume              .~ lFromJSInt            (get_field jsonput "vol") $
+    strike              .~ lFromMaybeJSDouble "strike" (get_field jsonput "strike") $
+    expiry              .~ lDateFromString        (lFromJSString "expiry" $ get_field jsonput "expiry") $
     underlyingSpotprice .~ spot $
     uninitialisedCall
 
 
--- converting a JSON object representing a date to type Day
-fromJSDate jsdate = fromGregorian (fromJSRational y) (fromJSRational m) (fromJSRational d)
+-- | converting a JSON object representing a date to type Day
+lFromJSDate :: JSObject JSValue -> Day
+lFromJSDate jsdate = fromGregorian (lFromJSRational y) (lFromJSRational m) (lFromJSRational d)
     where Just y = get_field jsdate "y"
           Just m = get_field jsdate "m"
           Just d = get_field jsdate "d"
 
--- Converts a Maybe JSValue representing a potential string to a String
-fromMyString err = fromJSString . fromJSSTRING . fromMaybe (error $ "error processing JSValue String " ++ err)
+-- | Converts a Maybe JSValue representing a potential string to a String
+lFromJSString :: String -> Maybe JSValue -> String
+lFromJSString err = fromJSString . lFromJSSTRING . fromMaybe (error $ "error processing JSValue String " ++ err)
 
--- Converts a Maybe JSValue representing a potential double to a Double
-fromMyDouble :: String -> Maybe JSValue -> Double 
-fromMyDouble err = read . fromJSString . fromJSSTRING . fromMaybe (error $ "error processing JSValue Double " ++ err)
+-- | Converts a Maybe JSValue representing a potential double to a Double
+lFromMaybeJSDouble :: String -> Maybe JSValue -> Double 
+lFromMaybeJSDouble err = read . fromJSString . lFromJSSTRING . fromMaybe (error $ "error processing JSValue Double " ++ err)
 
--- Converts a Maybe JSValue representing a potential int to an Int
-fromMyInt :: String -> Maybe JSValue -> Int
-fromMyInt err = read . fromJSString . fromJSSTRING . fromMaybe (error $ "error processing JSValue Int " ++ err)
+-- | Converts a Maybe JSValue representing a potential int to an Int
+lFromMaybeJSInt :: String -> Maybe JSValue -> Int
+lFromMaybeJSInt err = read . fromJSString . lFromJSSTRING . fromMaybe (error $ "error processing JSValue Int " ++ err)
 
--- Converts a Maybe JSValue representing a potential double to a Maybe Double
-fromMyDouble' :: Maybe JSValue -> Maybe Double 
-fromMyDouble' x = do y <- x 
-                     let z = fromJSString $ fromJSSTRING y
-                     if z == "-" || null z 
-                       then fail "-"
-                       else (
-                          if head z == '+' then
-                              return $ read $ tail z 
-                          else
-                              return $ read z 
-                          )
+-- | Converts a Maybe JSValue representing a potential double to a Maybe Double
+lFromJSDouble  :: Maybe JSValue -> Maybe Double 
+lFromJSDouble  x = do y <- x 
+                      let z = fromJSString $ lFromJSSTRING y
+                      if z == "-" || null z 
+                        then fail "-"
+                        else (
+                           if head z == '+' then
+                               return $ read $ tail z 
+                           else
+                               return $ read z 
+                           )
 
--- Converts a Maybe JSValue representing a potential int to a Maybe Int                    
-fromMyInt' :: Maybe JSValue -> Maybe Int 
-fromMyInt' x = do y <- x 
-                  let z = fromJSString $ fromJSSTRING y
+-- | Converts a Maybe JSValue representing a potential int to a Maybe Int                    
+lFromJSInt :: Maybe JSValue -> Maybe Int 
+lFromJSInt x = do y <- x 
+                  let z = fromJSString $ lFromJSSTRING y
                   if z == "-" 
                     then fail "-"
                     else return $ read z
 
 -- unwrapping various JSValue constructors
-fromJSRational (JSRational _ x) = floor x
-fromJSSTRING (JSString x) = x
-fromJSOBJECT (JSObject x) = x
+lFromJSRational (JSRational _ x) = floor x
+lFromJSSTRING (JSString x) = x
+lFromJSOBJECT (JSObject x) = x
 
 -- converting a string representing a date in "MMM DD, YYYY" format to a date of type Day
-dateFromString :: String -> Day 
-dateFromString stringdate = either (\e -> error $ "error processing JSValue Day: " ++ show e) (\(y,m,d) -> fromGregorian y m d) $ runParser date_p () "stdin" stringdate
+lDateFromString :: String -> Day 
+lDateFromString stringdate = either (\e -> error $ "error processing JSValue Day: " ++ show e) (\(y,m,d) -> fromGregorian y m d) $ runParser date_p () "stdin" stringdate
     where date_p = do m <- month_p
                       _ <- spaces
                       d <- day_p
